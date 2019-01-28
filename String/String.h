d76b073f45db3ac69b4499b9eb6bb4a7c9ce7cce
Matthew Day <daystar.1998@yahoo.com>
2019-01-28 12:25:24 -0600

    Add String project with teacher provided code

---
+++ String/String.h
@@ -0,0 +1,51 @@
+#include<iostream>
+
+class String {
+
+private:
+
+	char* pString;
+public:
+
+	String();
+
+	String(const String& newString);
+
+	String(const char* const newString);
+
+	//String( int i );
+
+	//String( char c );
+
+	~String();
+
+	void set(const char* const newString);
+
+	void set(const String& newString);
+
+	//void set( int i );
+
+	//void set( char c );
+
+	void write(std::ostream& out);
+
+	// append takes an int, char, char* or String&
+	// prepend takes an int, char , char* or String&
+
+	// bool contains( char c);
+
+	// bool contains( String s );
+
+	// bool contains( const char* const s );
+
+	// bool contains( int i );
+
+	// void insert String, char*, int, char, at an index
+
+	bool isEmpty(); // empty when the pointer is null or the first character is \0
+	
+	int length();
+private:
+
+	void destroy();
+};
