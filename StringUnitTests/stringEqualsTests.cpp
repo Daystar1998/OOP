7589dc22636d74432ac26af2e21139ac2d4e9468
Matthew Day <daystar.1998@yahoo.com>
2019-02-01 23:32:41 -0600

    Add unit testing for equals function

---
+++ StringUnitTests/stringEqualsTests.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../String/src/String.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StringUnitTests
+{		
+	TEST_CLASS(EqualsTests)
+	{
+	public:
+		
+		TEST_METHOD(StringEqualsSuccess)
+		{
+			String s = String("Test");
+
+			if (!s.equals(String("Test"))) {
+
+				Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(StringEqualsFailBeginning) {
+			String s = String("Test");
+
+			if (s.equals(String("fTest"))) {
+
+				Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(StringEqualsFailEnding) {
+			String s = String("Test");
+
+			if (s.equals(String("Testf"))) {
+
+				Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(StringEqualsFailMiddle) {
+			String s = String("Test");
+
+			if (s.equals(String("Tefst"))) {
+
+				Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(StringEqualsFailBothEnds) {
+			String s = String("Test");
+
+			if (s.equals(String("fTestf"))) {
+
+				Assert::Fail();
+			}
+		}
+
+		TEST_METHOD(StringEqualsFailBothEndsAndMiddle) {
+			String s = String("Test");
+
+			if (s.equals(String("fTefstf"))) {
+
+				Assert::Fail();
+			}
+		}
+	};
+}
