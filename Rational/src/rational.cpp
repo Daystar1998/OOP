3549dbfc427c8ac3ec7121f265e64b8217de93c4
Matthew Day <daystar.1998@yahoo.com>
2019-02-04 23:15:29 -0600

    Add teacher-provided code

--- Rational/src/rational.cpp
+++ Rational/src/rational.cpp
@@ -1,6 +1,113 @@
 #include "rational.h"
 
-Rational::Rational() {
+// Author: Dana Steil
+const Rational& Rational::operator=(const Rational &rValue) {
 
+	numerator = rValue.numerator;
+	denominator = rValue.denominator;
+	reduce();
 
+	return *this;
+}
+
+// Author: Dana Steil
+Rational& Rational::operator++() {
+
+	numerator += denominator;
+	reduce();
+
+	return *this;
+}
+
+// Author: Dana Steil
+Rational Rational::operator++(int) {
+
+	Rational result = *this;
+	numerator += denominator;
+
+	reduce();
+
+	return result;
+}
+
+// Author: Dana Steil
+bool Rational::operator==(const Rational & rValue) const {
+
+	bool result = true;
+
+	if (numerator != rValue.numerator || denominator != rValue.denominator) {
+
+		result = false;
+	}
+
+	return result;
+}
+
+// Author: Dana Steil
+string Rational::operator()() const {
+
+	std::stringstream stream;
+	stream << numerator << "/" << denominator;
+
+	return stream.str();
+}
+
+// Author: Dana Steil
+long Rational::getNumerator() const {
+
+	return numerator;
+}
+
+// Author: Dana Steil
+long Rational::getDenominator() const {
+
+	return denominator;
+}
+
+// Author: Dana Steil
+long Rational::leastCommonMultiple(long x, long y) const {
+
+	bool Continue = true;
+
+	long result = x;
+
+	while (result % y != 0) {
+
+		result += x;
+	}
+
+	return result;
+}
+
+// Author: Dana Steil
+long Rational::greatestCommonDivisor(long x, long y) const {
+
+	long remainder = x % y;
+
+	while (remainder != 0) {
+
+		x = y;
+		y = remainder;
+
+		remainder = x % y;
+	}
+
+	return y;
+}
+
+// Author: Dana Steil
+void Rational::reduce() {
+
+	long GCD = greatestCommonDivisor(numerator, denominator);
+
+	numerator = numerator / GCD;
+	denominator = denominator / GCD;
+}
+
+// Author: Dana Steil
+ostream &operator<<(ostream & out, const Rational &rational) {
+
+	out << rational.getNumerator() << "\\" << rational.getDenominator();
+
+	return out;
 }
--- Rational/src/rational.h
+++ Rational/src/rational.h
@@ -1,9 +1,40 @@
 #pragma once
 
+#include <sstream>
+#include <string>
+
+using std::ostream;
+using std::string;
+
 class Rational {
 
+private:
+
+	long numerator;
+	long denominator;
 public:
 
-	Rational();
+	const Rational& operator=(const Rational &rValue);
+
+	Rational& operator++();
+
+	Rational operator++(int);
+
+	bool operator==(const Rational & rValue) const;
+
+	string operator()() const;
+
+	long leastCommonMultiple(long x, long y) const;
+
+	long greatestCommonDivisor(long x, long y) const;
+private:
+
+	void reduce();
+public:
+
+	long getNumerator() const;
+
+	long getDenominator() const;
 };
 
+ostream &operator<<(ostream & out, const Rational &rational);
