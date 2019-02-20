395e2ca1ea928f38a9a0abf1b64cc639e522b533
Matthew Day <daystar.1998@yahoo.com>
2019-02-20 12:04:55 -0600

    Add teacher provided code

---
+++ Shapes to BMP/src/RGBTriple.cpp
@@ -0,0 +1,10 @@
+// Author: Dana Steil
+
+#include"RGBTriple.h"
+
+RGBTriple::RGBTriple() {}
+RGBTriple::RGBTriple(unsigned char blue, unsigned char green, unsigned char red) {
+	Blue = blue;
+	Green = green;
+	Red = red;
+}
---
+++ Shapes to BMP/src/RGBTriple.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+// Author: Dana Steil
+
+struct RGBTriple {
+	unsigned char Blue;
+	unsigned char Green;
+	unsigned char Red;
+
+	RGBTriple();
+	RGBTriple(unsigned char blue, unsigned char green, unsigned char red);
+};
