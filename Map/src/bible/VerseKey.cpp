fe3acdaacd4ff3ac94642869d89fe9c1fc5c9b93
Matthew Day <daystar.1998@yahoo.com>
2019-03-20 10:33:31 -0500

    Add override for the comparison operator

--- Map/src/bible/VerseKey.cpp
+++ Map/src/bible/VerseKey.cpp
@@ -5,3 +5,10 @@
 VerseKey::VerseKey() {}
 
 VerseKey::~VerseKey() {}
+
+bool VerseKey::operator==(const VerseKey &other) const {
+
+	bool result = this->book == other.book && this->chapter == other.chapter && this->verseNumber == other.verseNumber;
+
+	return result;
+}
--- Map/src/bible/VerseKey.h
+++ Map/src/bible/VerseKey.h
@@ -2,12 +2,24 @@
 
 // Author: Matthew Day
 
+#include <string>
+
+using std::string;
+
 class VerseKey {
 
+private:
+
+	string book;
+
+	int chapter;
+	int verseNumber;
 public:
 
 	VerseKey();
 
 	virtual ~VerseKey();
+
+	bool operator==(const VerseKey &other) const;
 };
 
