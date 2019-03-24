3712ddcf0154fde917bdae0e3632df0e33dd669b
Matthew Day <daystar.1998@yahoo.com>
2019-03-24 14:20:12 -0500

    Implement BibleWordCounter's visit function

--- Map/src/bible/BibleWordCounter.cpp
+++ Map/src/bible/BibleWordCounter.cpp
@@ -2,6 +2,8 @@
 
 #include "BibleWordCounter.h"
 
+#include "../util/StringUtils.h"
+
 BibleWordCounter::BibleWordCounter(const string &word)
 	: word(word), count(0) {}
 
@@ -9,4 +11,19 @@ BibleWordCounter::~BibleWordCounter() {}
 
 void BibleWordCounter::visit(VerseKey &key, Verse &verse) {
 
+	string verseText = verse.getText();
+
+	int currentPosition = 0;
+
+	string currentWord = StringUtils::getNextVariable(verseText, currentPosition, currentPosition);
+
+	while (currentPosition < verseText.size()) {
+
+		if (currentWord == word) {
+
+			count++;
+		}
+
+		currentWord = StringUtils::getNextVariable(verseText, currentPosition + 1, currentPosition);
+	}
 }
