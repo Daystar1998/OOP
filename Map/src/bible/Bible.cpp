edcdf8d3e801c33e657c195ff8fe0e76096941de
Matthew Day <daystar.1998@yahoo.com>
2019-03-23 20:47:24 -0500

    Parse the book names from bible.txt

--- Map/src/bible/Bible.cpp
+++ Map/src/bible/Bible.cpp
@@ -2,9 +2,44 @@
 
 #include "Bible.h"
 
+#include <cctype>
+#include <fstream>
+
+#include "../util/StringUtils.h"
+
 Bible *Bible::instance = nullptr;
 
-Bible::Bible() {}
+Bible::Bible() {
+
+	std::fstream fin("res/bible.txt");
+
+	string line;
+
+	string currentBookName;
+
+	while (!fin.eof()) {
+
+		std::getline(fin, line);
+
+		if (StringUtils::startsWith(line, "Book")) {
+
+			// Start at index after "Book"
+			int currentPosition = 4;
+
+			// Skip the book number
+			StringUtils::getNextNumber(line, currentPosition, currentPosition);
+
+			currentBookName = StringUtils::getNextVariable(line, currentPosition + 1, currentPosition);
+
+			if (std::isdigit(currentBookName[0])) {
+
+				currentBookName += " ";
+
+				currentBookName.append(StringUtils::getNextVariable(line, currentPosition + 1, currentPosition));
+			}
+		}
+	}
+}
 
 Bible::~Bible() {}
 
