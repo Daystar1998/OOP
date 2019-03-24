acb4eb1797aa40dec751b3a79eba40eda35c26e6
Matthew Day <daystar.1998@yahoo.com>
2019-03-24 16:57:19 -0500

    Add simple menu

--- Map/src/main.cpp
+++ Map/src/main.cpp
@@ -1,10 +1,43 @@
 // Author: Matthew Day
 
+#include <iostream>
+
 #include "bibleSearcher.h"
+
+using std::cin;
+using std::endl;
+using std::cout;
+
+int searchOption = 1;
+int iterateOption = 2;
+int quitOption = 3;
+
+void printMenu() {
+
+	cout << searchOption << ": Search Bible for verse" << endl;
+	cout << iterateOption << ": Iterate over Bible" << endl;
+	cout << quitOption << ": Quit" << endl;
+	cout <<  "\nEnter selection: ";
+}
 
 int main() {
 
-	searchBible();
+	int selectedOption;
+
+	do {
+
+		printMenu();
+
+		cin >> selectedOption;
+
+		if (selectedOption == searchOption) {
+
+			searchBible();
+		} else if (selectedOption == iterateOption) {
+
+
+		}
+	} while (selectedOption != quitOption);
 
 	return 0;
 }
