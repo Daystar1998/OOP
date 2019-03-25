e32f5b09a8bf176c961107bf7f5585223e74db63
Matthew Day <daystar.1998@yahoo.com>
2019-03-25 11:56:10 -0500

    Add error checks for loading the file

--- Map/src/bible/Bible.cpp
+++ Map/src/bible/Bible.cpp
@@ -13,50 +13,55 @@ Bible::Bible() {
 
 	std::fstream fin("res/bible.txt");
 
-	string line;
+	if (!fin.fail()) {
 
-	string currentBookName;
+		string line;
+		string currentBookName;
 
-	while (!fin.eof()) {
+		while (!fin.eof() && !fin.fail()) {
 
-		std::getline(fin, line);
+			std::getline(fin, line);
 
-		if (StringUtils::startsWith(line, "Book")) {
+			if (StringUtils::startsWith(line, "Book")) {
 
-			// Start at index after "Book"
-			int currentPosition = 4;
+				// Start at index after "Book"
+				int currentPosition = 4;
 
-			// Skip the book number
-			StringUtils::getNextNumber(line, currentPosition, currentPosition);
+				// Skip the book number
+				StringUtils::getNextNumber(line, currentPosition, currentPosition);
 
-			currentBookName = StringUtils::getNextVariable(line, currentPosition + 1, currentPosition);
+				currentBookName = StringUtils::getNextVariable(line, currentPosition + 1, currentPosition);
 
-			if (std::isdigit(currentBookName[0])) {
+				if (std::isdigit(currentBookName[0])) {
 
-				currentBookName += " ";
+					currentBookName += " ";
 
-				currentBookName.append(StringUtils::getNextVariable(line, currentPosition + 1, currentPosition));
+					currentBookName.append(StringUtils::getNextVariable(line, currentPosition + 1, currentPosition));
+				}
+			} else {
+
+				int chapter;
+				int verseNumber;
+				string verseText;
+
+				int currentPosition = 0;
+
+				chapter = StringUtils::getNextNumber(line, currentPosition, currentPosition);
+
+				verseNumber = StringUtils::getNextNumber(line, currentPosition + 1, currentPosition);
+
+				verseText = line.substr(currentPosition + 1, line.length() - currentPosition - 1);
+
+				VerseKey verseKey(currentBookName, chapter, verseNumber);
+
+				StringUtils::trim(verseText);
+
+				(*this)[verseKey] = Verse(verseKey, verseText);
 			}
-		} else {
-
-			int chapter;
-			int verseNumber;
-			string verseText;
-
-			int currentPosition = 0;
-
-			chapter = StringUtils::getNextNumber(line, currentPosition, currentPosition);
-
-			verseNumber = StringUtils::getNextNumber(line, currentPosition + 1, currentPosition);
-
-			verseText = line.substr(currentPosition + 1, line.length() - currentPosition - 1);
-
-			VerseKey verseKey(currentBookName, chapter, verseNumber);
-
-			StringUtils::trim(verseText);
-
-			(*this)[verseKey] = Verse(verseKey, verseText);
 		}
+	} else {
+
+		throw std::runtime_error("File not found");
 	}
 }
 
